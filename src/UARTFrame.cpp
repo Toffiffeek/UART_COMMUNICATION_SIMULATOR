cc9941dc5764b6651f970ffe14169310c0113649
Miki <mik.rutecki@gmail.com>
2026-08-06 22:46:05 +0200

    fixed a bug

--- src/UARTFrame.cpp
+++ src/UARTFrame.cpp
@@ -13,15 +13,18 @@ UARTFrame::UARTFrame(char character, const UARTConfig& config):character(charact
 char UARTFrame::getCharacter() const{
     return character;
 }
+
 const std::vector<bool>& UARTFrame::getBits() const{
     return bits;
 }
+
 void UARTFrame::characterToBits(char character, std::vector<bool>& frame){
     std::bitset<8> bs(character);
     for(int i = 0; i < 8; i++){
         frame.push_back(bs[i]);
     }
 }
+
 bool UARTFrame::calculateParityBit(const std::vector<bool>& dataBits){
     int numberOfOnes = 0;
     for(int i = 0; i < 8; i++){
@@ -40,14 +43,21 @@ void UARTFrame::addStopBits(std::vector<bool>& frame, const UARTConfig& config){
     switch(config.getStopBits()){
         case StopBits::One:
             frame.push_back(1);
+            break;
         case StopBits::Two:
             for(int i = 0; i < 2; i++){
                 frame.push_back(1);
             }
+            break;
         default:
             throw std::invalid_argument("INVALID VALUE");
     }
 }
 
-void UARTFrame::addParityBit(std::vector<bool>& frame, const UARTConfig& config){}
+void UARTFrame::addParityBit(std::vector<bool>& frame, const UARTConfig& config){
+    switch(config.getParity()){
+        case ParityMode::None
+    }
+}
+
 std::vector<bool> UARTFrame::buildFrame(std::vector<bool>& frame, const UARTConfig& config){}
